7d6f05cce919f261e9694cdce11bef019212de76
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-10-26 18:17:36 +0200

    added unbounded simple example

--- src/com/samodeika/wildcards/WildcardsExamples.java
+++ src/com/samodeika/wildcards/WildcardsExamples.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class WildcardsExamples {
 
@@ -15,11 +16,15 @@ public class WildcardsExamples {
         partners.add(alexNik);
         partners.add(stoPan);
 
-        PersonSaver saver = new PersonSaver(new File("abvv", "rw"));
+        //PersonSaver saver = new PersonSaver(new File("abvv", "rw"));
+        //saver.save(alexNik);
+        //saver.saveAll(partners);
 
-        saver.save(alexNik);
-
-        saver.saveAll(partners);
+        // Unbounded usage
+        List<? super Object> objects = new ArrayList<>();
+        objects.add(new Object());
+        objects.add(new Person("Pesho Peshov", 38));
+        System.out.println(objects);
     }
 
 }
